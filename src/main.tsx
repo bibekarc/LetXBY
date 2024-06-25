2b63cbb2f2d13fb300083cfe4c4399fa4a58e7ee
Bibek <bibekz2008@gmail.com>
2024-06-25 19:24:06 +0545

    added logo in topbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,6 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-
-// import { AuthProvider } from "@/context/AuthContext";
-
-// import App from "./App";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <QueryClientProvider client={QueryClient}>
-//         <AuthProvider>
-//           <App />
-//         </AuthProvider>
-//       </QueryClientProvider>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import { AuthProvider } from "@/context/AuthContext";
 import { QueryProvider } from "@/lib/react-query/QueryProvider";
 
